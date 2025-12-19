bd83f1ff262c4e337c5d3efe61b855a1496617fa
Erick <erick@coesolutions.com>
2025-12-18 19:48:18 -0600

    Added my introduction

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 # Hello AI/ML Bootcamp!
 # Edit the variables below to introduce yourself
 
-name = "Your Name Here"
+name = "Erick Lanford"
 fun_fact = "One fun fact about me"
 
 # Don't change anything below this line
-print(f"Hello! I'm {name}.")
+print(f"Hello all! I'm {name}.")
 print(f"Fun fact: {fun_fact}")
 print("I'm excited to learn AI/ML!")
